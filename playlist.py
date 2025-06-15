9fdf634a2375d5a37b7bf3cde35e1b1922aa7c88
@Mohammad_Dev <ouyoussefmohamedreda2@gmail.com>
2025-06-15 19:39:03 +0100

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -5,8 +5,8 @@ playlist_url = input("enter url playlist video in youtube: ")
 try:
     pl = Playlist(playlist_url)
 
-    print(f"جارٍ تنزيل قائمة التشغيل: {pl.title}")
-    print(f"تحتوي على {len(pl.video_urls)} فيديو")
+    print(f"download video...: {pl.title}")
+    print(f"{len(pl.video_urls)} video")
 
     for index, video in enumerate(pl.videos, start=1):
         print(f"\n[{index}/{len(pl.video_urls)}] Downloaded playlist: {video.title}")
