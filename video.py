d01fc6c5fb9b3d6a2d20d93dc327cb0a2eed4b7c
@Mohammad_Dev <ouyoussefmohamedreda2@gmail.com>
2025-06-15 12:25:37 +0100

    Create video.py

---
+++ video.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+
+url = input("enter url video in youtube: ")
+
+try:
+    yt = YouTube(url)
+    
+    video_stream = yt.streams.get_highest_resolution()
+    
+    print(f"don: {yt.title}")
+    video_stream.download()
+    
+    print("✅ Downloaded successfully!")
+
+except Exception as e:
+    print(f"❌ An error occurred while downloading: {e}")
