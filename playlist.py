5634e0f686fb37300b67410015f16300299333a6
@Mohammad_Dev <ouyoussefmohamedreda2@gmail.com>
2025-06-15 12:37:06 +0100

    Create playlist.py

---
+++ playlist.py
@@ -0,0 +1,18 @@
+from pytube import Playlist
+
+playlist_url = input("أدخل رابط قائمة التشغيل من يوتيوب: ")
+
+try:
+    pl = Playlist(playlist_url)
+
+    print(f"جارٍ تنزيل قائمة التشغيل: {pl.title}")
+    print(f"تحتوي على {len(pl.video_urls)} فيديو")
+
+    for index, video in enumerate(pl.videos, start=1):
+        print(f"\n[{index}/{len(pl.video_urls)}] Downloaded playlist: {video.title}")
+        video.streams.get_highest_resolution().download(output_path="Playlist_Downloads")
+
+    print("\n✅ Downloaded successfully!")
+
+except Exception as e:
+    print(f"❌ An error occurred while downloading: {e}")
